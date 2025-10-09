97940de6383042013d3576fe58ff88f063964130
Natural <itsmrnatural@duck.com>
2025-10-09 10:14:30 +0530

    style: align pointer symbols to the left

--- include/utils.h
+++ include/utils.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-inline void readline(char *line, size_t size) {
+inline void readline(char* line, size_t size) {
     if (fgets(line, size, stdin)) {
         line[strcspn(line, "\n")] = '\0';  // strip newline
     }
