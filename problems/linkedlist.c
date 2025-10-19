a55cd4052290f8ad4aaa9ce9dec3498395baf42c
Natural <itsmrnatural@duck.com>
2025-10-19 20:59:41 +0530

    fix: implement delete node and prune nodes functionality

--- problems/linkedlist.c
+++ problems/linkedlist.c
@@ -1,16 +1,3 @@
-/*
-    Requirements:
-    Node structure: value and next pointer
-    Operations:
-        Insert at head
-        Insert at tail
-        Delete a node by value
-        Search for a value
-        Print the list
-        Free all nodes (avoid memory leaks)
-        Test each operation thoroughly
-*/
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,36 +6,35 @@
 // Type definations
 typedef struct node_t {
     char* value;
-    /// For the next to be declared as a node_t pointer we need to name the struct, no anonymous naming.
+    /// For the next to be declared as a node_t pointer
+    //  we need to name the struct, no anonymous naming.
     struct node_t* next;
 } node_t;
 // typedef node_t* node_ptr; // Unused!
 
 // Function Prototypes
-/// General
 bool compare(const char*, const char*);
 
-/// Node specific
 void print_nodes(node_t*);
 node_t* create_node(char*);
 node_t* insert_at_head(node_t*, node_t*);
 node_t* insert_at_tail(node_t*, node_t*);
 node_t* search_by_value(node_t*, const char*);
 node_t* delete_by_value(node_t*, const char*);
-node_t* prune_all_nodes(node_t*);
+void prune_all_nodes(node_t*);
 
 // Function Definations
 int main(void) {
     node_t* head_ptr = NULL;  // [HEAD] -> [NULL]
 
-    printf("Linked List Program Simulation");
-    printf("\t1. Print all nodes");
-    printf("\t2. Insert at head");
-    printf("\t3. Insert at tail");
-    printf("\t4. Search by value");
-    printf("\t5. Delete by value");
-    printf("\t6. Prune all nodes");
-    printf("\t7. Exit");
+    printf("Linked List Program Simulation\n");
+    printf("\t1. Print all nodes\n");
+    printf("\t2. Insert at head\n");
+    printf("\t3. Insert at tail\n");
+    printf("\t4. Search by value\n");
+    printf("\t5. Delete by value\n");
+    printf("\t6. Prune all nodes\n");
+    printf("\t7. Exit\n");
     // ... to be made ...
     return 0;
 }
@@ -137,19 +123,40 @@ node_t* search_by_value(node_t* head_ptr, const char* value) {
 }
 
 node_t* delete_by_value(node_t* head_ptr, const char* value) {
-    //* For safety returns the head pointer after the delete operation
+    //* Returns pointer to the element before the element deleted
+    //* Returns NULL pointer if operation failed
     if (!head_ptr) {
         //
         return NULL;
     }
 
-    node_t* current_node = head_ptr;
+    node_t* prev_node = head_ptr;     // Using as a default pointer to
+    node_t* current_node = head_ptr;  // return if first node itself matches
 
     while (current_node != NULL) {
         if (compare(current_node->value, value)) {
+            // This connects the previous node to the next node
+            // Disconnecting the current one
+            prev_node = current_node->next;
+            free(current_node);
+            return prev_node;
         }
+        prev_node = current_node;
+        current_node = current_node->next;
     }
+
+    return NULL;
 }
 
-node_t* prune_all_nodes(node_t* head_ptr) {
+void prune_all_nodes(node_t* head_ptr) {
+    if (!head_ptr) {
+        //
+        return;
+    }
+
+    node_t* current_ptr = head_ptr;
+    while (current_ptr != NULL) {
+        // current_ptr gets head_ptr each time
+        current_ptr = delete_by_value(current_ptr, current_ptr->value);
+    }
 }
