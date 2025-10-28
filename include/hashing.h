cb7301d7ece323a75a57e420eafdaeef4ab0b4f8
Natural <itsmrnatural@duck.com>
2025-10-28 16:49:30 +0530

    chore: add hashing header file

---
+++ include/hashing.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdint.h>
+
+uint64_t fnv1a_hash(const char* key);
--- src/fnv-1a.c
+++ src/fnv-1a.c
@@ -1,3 +1,5 @@
+#include "../include/hashing.h"
+
 #include <stdint.h>
 
 /*
@@ -8,7 +10,7 @@ standard in FNV-1a hash for 64-bit systems.
 #define FNV_PRIME 1099511628211UL
 
 // Return 64-bit FNV-1a hash for key (NUL-terminated)
-static uint64_t hash(const char* key) {
+uint64_t fnv1a_hash(const char* key) {
     uint64_t hashed = FNV_OFFSET;
     for (const char* p = key; *p; p++) {
         hashed ^= (uint64_t) (unsigned char) *p;
