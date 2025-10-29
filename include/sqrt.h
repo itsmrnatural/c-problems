5b3bd385bd2734b296d125319e2e31f557fceae0
Natural <itsmrnatural@duck.com>
2025-10-29 19:29:43 +0530

    feat: add three sqrt calculation algorithms
    Added Binary Search sqrt, Quake III sqrt and
    Newton-Raphson sqrt

---
+++ include/sqrt.h
@@ -0,0 +1,8 @@
+#ifndef SQRT_H
+#define SQRT_H
+
+float sqrt_srch(float num);
+float sqrt_quake(float num);
+float sqrt_nwtn(float num);
+
+#endif /* SQRT_H */
---
+++ utils/sqrt.c
@@ -0,0 +1,79 @@
+#include "../include/sqrt.h"
+
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SQRT_EPSILON 0.00001f  // degree of precision
+
+float sqrt_srch(float num) {
+    if (num < 0) return NAN;
+    if (num == 0) return 0;
+
+    float low = 0.0f;              // bit manipulation can only be
+    float high = num;              // performed on integer types
+    float epsilon = SQRT_EPSILON;  // error of 10^-5
+
+    if (num < 1) {
+        high = 1.0f;  // root 0.025 = 0.5
+    }
+
+    float mid;
+    while ((high - low) > epsilon) {
+        mid = low + ((high - low) / 2.0f);
+        if ((mid * mid) > num) {
+            high = mid;
+        } else {
+            low = mid;
+        }
+    }
+
+    return mid;  // calculated sqrt
+}
+
+float sqrt_quake(float num) {
+    if (num < 0) return NAN;
+    if (num == 0) return 0;
+
+    uint32_t bits;
+    memcpy(&bits, &num, 4);  // copy 4 bytes from address of num to i
+    uint32_t quake = 0x5f3759DF;
+
+    // log(num) = num + error (quake)
+    float root;
+    bits = quake - (bits >> 1);
+    memcpy(&root, &bits, 4);
+
+    float epsilon = SQRT_EPSILON;
+    double prev, droot = (double) root;
+
+    do {
+        prev = droot;
+        droot = droot * (1.5f - 0.5f * num * droot * droot);
+    } while (fabs(droot - prev) > epsilon);
+
+    return num * droot;
+}
+
+float sqrt_nwtn(float num) {
+    if (num < 0) return NAN;
+    if (num == 0) return 0;
+
+    // Guessing root using bit manipulation
+    uint32_t bits;
+    memcpy(&bits, &num, 4);
+    bits = 0x1fbd1df5 + (bits >> 1);
+    float guess;
+    memcpy(&guess, &bits, 4);
+
+    double epsilon = SQRT_EPSILON;  // degree of precision
+    double i, j = guess;
+
+    do {
+        i = j;  // i is j before NR iteration
+        j = 0.5f * (i + (num / i));
+    } while (fabs(i - j) > epsilon);
+
+    return j;
+}
